f1970da433e1c50253212c856ccd4af99e8b285b
Francis Robichaud <francis@lime.health>
2025-08-22 17:50:13 -0400

    WIP: temp HW2 fingerprints

--- opendbc_repo/opendbc/car/tesla/fingerprints.py
+++ opendbc_repo/opendbc/car/tesla/fingerprints.py
@@ -64,10 +64,10 @@ FW_VERSIONS = {
   CAR.TESLA_MODEL_S_HW2: {
     (Ecu.eps, 0x730, None): [
       b'\x10#\x01',
-    ],
-  },
-  CAR.TESLA_MODEL_S_HW3: {
-    (Ecu.eps, 0x730, None): [
+#    ],
+#  },
+#  CAR.TESLA_MODEL_S_HW3: {
+#    (Ecu.eps, 0x730, None): [
       b'SX_0.0.0 (99),S013.7',
       b'SX_0.0.0 (99),SR013.7',
     ],
