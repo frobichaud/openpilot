8c4e9693aae138daf307602041df15760f6a2571
James <91348155+FrogAi@users.noreply.github.com>
2026-03-04 15:29:19 -0700

    FrogPilot base

--- selfdrive/locationd/lagd.py
+++ selfdrive/locationd/lagd.py
@@ -394,6 +394,8 @@ def main():
     lag, valid_blocks = initial_lag_params
     lag_learner.reset(lag, valid_blocks)
 
+  # FrogPilot variables
+
   while True:
     sm.update()
     if sm.all_checks():
@@ -412,3 +414,5 @@ def main():
 
       if sm.frame % 1200 == 0: # cache every 60 seconds
         params.put_nonblocking("LiveDelay", lag_msg_dat)
+
+    # FrogPilot variables
